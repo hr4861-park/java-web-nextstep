83fd19159bbf684832c21a17ea7ca15eb55a3bb4
Hyangrae Park <hyangrae.park@gmail.com>
2023-12-26 21:28:37 +0900

    :sparkles: index.html 불러오도록 변경

--- Chapter3/src/main/java/webserver/RequestHandler.java
+++ Chapter3/src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,20 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
+
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private final Socket connection;
@@ -24,9 +29,14 @@ public class RequestHandler extends Thread {
             connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String line = bufferedReader.readLine();
+
+            String[] splited = line.split(" ");
+            String path = splited[1];
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
