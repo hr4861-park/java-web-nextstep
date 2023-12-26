3898a05d15cdf2450b656cf609941af9434f2288
Hyangrae Park <hyangrae.park@gmail.com>
2023-12-26 18:25:08 +0900

    :tada: 프로젝트 시작

---
+++ Chapter3/src/main/java/db/DataBase.java
@@ -0,0 +1,27 @@
+package db;
+
+import com.google.common.collect.Maps;
+import java.util.Collection;
+import java.util.Map;
+import model.User;
+
+public class DataBase {
+
+    private static final Map<String, User> users = Maps.newHashMap();
+
+    private DataBase() {
+
+    }
+
+    public static void addUser(User user) {
+        users.put(user.getUserId(), user);
+    }
+
+    public static User findUserById(String userId) {
+        return users.get(userId);
+    }
+
+    public static Collection<User> findAll() {
+        return users.values();
+    }
+}
---
+++ Chapter3/src/main/java/model/User.java
@@ -0,0 +1,35 @@
+package model;
+
+public class User {
+
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+            "userId='" + userId + '\'' +
+            ", password='" + password + '\'' +
+            ", name='" + name + '\'' +
+            ", email='" + email + '\'' +
+            '}';
+    }
+}
