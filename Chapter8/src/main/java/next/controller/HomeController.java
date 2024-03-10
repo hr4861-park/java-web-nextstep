414eed101d29b9c93c9a0cfc60a0a9ba6d570f82
Hyangrae Park <hyangrae.park@gmail.com>
2024-03-10 15:16:54 +0900

    :sparkles: Chapter8 실습 코드

---
+++ Chapter8/src/main/java/next/controller/HomeController.java
@@ -0,0 +1,17 @@
+package next.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import next.dao.QuestionDao;
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+
+public class HomeController extends AbstractController {
+    private QuestionDao questionDao = new QuestionDao();
+
+    @Override
+    public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return jspView("home.jsp").addObject("questions", questionDao.findAll());
+    }
+}
---
+++ Chapter8/src/main/java/next/controller/qna/ShowController.java
@@ -0,0 +1,24 @@
+package next.controller.qna;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import next.dao.AnswerDao;
+import next.dao.QuestionDao;
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+
+public class ShowController extends AbstractController {
+    private QuestionDao questionDao = new QuestionDao();
+    private AnswerDao answerDao = new AnswerDao();
+
+    @Override
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
+        Long questionId = Long.parseLong(req.getParameter("questionId"));
+
+        ModelAndView mav = jspView("/qna/show.jsp");
+        mav.addObject("question", questionDao.findById(questionId));
+        mav.addObject("answers", answerDao.findAllByQuestionId(questionId));
+        return mav;
+    }
+}
